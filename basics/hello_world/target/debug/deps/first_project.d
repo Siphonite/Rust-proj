83410c4f2f6acf66587f2ac889d7a527d08b39bb
Aman Kumar <amansinha327@gmail.com>
2025-10-08 16:34:24 +0530

    returctred folders

---
+++ basics/hello_world/target/debug/deps/first_project.d
@@ -0,0 +1,5 @@
+C:\Users\HP\OneDrive\Desktop\rust_proj\first_project\basics\hello_world\target\debug\deps\first_project.d: src\main.rs
+
+C:\Users\HP\OneDrive\Desktop\rust_proj\first_project\basics\hello_world\target\debug\deps\first_project.exe: src\main.rs
+
+src\main.rs:
---
+++ basics/hello_world/target/debug/first_project.d
@@ -0,0 +1,1 @@
+C:\Users\HP\OneDrive\Desktop\rust_proj\first_project\basics\hello_world\target\debug\first_project.exe: C:\Users\HP\OneDrive\Desktop\rust_proj\first_project\basics\hello_world\src\main.rs
